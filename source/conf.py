9240ed265b2b535a7a449d4c37d24ef3cf1015df
tranvantoan <vantoan@planday.com>
2019-09-18 00:21:55 +0700

    change master doc to index.rst

--- source/conf.py
+++ source/conf.py
@@ -50,3 +50,7 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Set master doc to index.rst since by default, master doc will be contents.rst.
+# if we dont have that file, the build will fail
+master_doc = 'index'
