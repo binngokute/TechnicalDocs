98d608d7dabeb4b3650cd6f85dd7da8c8ac84a90
tranvantoan <vantoan@planday.com>
2019-09-18 16:48:09 +0700

    add custom css

--- source/conf.py
+++ source/conf.py
@@ -28,6 +28,7 @@ author = 'Toan Tran Van'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -44,12 +45,15 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Add custom stylesheet to document
+html_css_files = ['custom-style.css']
 
 # Set master doc to index.rst since by default, master doc will be contents.rst.
 # if we dont have that file, the build will fail
